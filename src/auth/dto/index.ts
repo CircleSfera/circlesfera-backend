8769435273b57ac912706ca5df8c895d3a7ed612
ShadyFeliu <shadyfeliu@gmail.com>
2026-04-02 05:10:15 +0200

    feat: secure authentication system with JWT/Passkeys

---
+++ src/auth/dto/index.ts
@@ -0,0 +1,6 @@
+export * from './login.dto.js';
+export * from './register.dto.js';
+export * from './refresh-token.dto.js';
+export * from './request-reset.dto.js';
+export * from './reset-password.dto.js';
+export * from './verify-email.dto.js';
